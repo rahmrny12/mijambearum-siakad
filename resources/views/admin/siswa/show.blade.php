f88f700c4ae7734cd1fa1df0fa0781695c8b6c97
rahmrny12 <rahmatprayogo12@gmail.com>
2024-08-31 15:10:56 +0700

    fix: show nisn instead

--- resources/views/admin/siswa/show.blade.php
+++ resources/views/admin/siswa/show.blade.php
@@ -19,7 +19,7 @@
                 <tr>
                     <th>No.</th>
                     <th>Nama Siswa</th>
-                    <th>No Induk</th>
+                    <th>NISN</th>
                     <th>Foto</th>
                     <th>Aksi</th>
                 </tr>
@@ -29,7 +29,7 @@
                     <tr>
                         <td>{{ $loop->iteration }}</td>
                         <td>{{ $data->nama_siswa }}</td>
-                        <td>{{ $data->no_induk }}</td>
+                        <td>{{ $data->nis }}</td>
                         <td>
                             <a href="{{ asset($data->foto) }}" data-toggle="lightbox" data-title="Foto {{ $data->nama_siswa }}" data-gallery="gallery" data-footer='<a href="{{ route('siswa.ubah-foto', Crypt::encrypt($data->id)) }}" id="linkFotoGuru" class="btn btn-link btn-block btn-light"><i class="nav-icon fas fa-file-upload"></i> &nbsp; Ubah Foto</a>'>
                                 <img src="{{ asset($data->foto) }}" width="130px" class="img-fluid mb-2">
